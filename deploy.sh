b12223987911ed5d25b662c32b2ed082997ceddd
dpny518 <dhonampemba@Dhonams-MacBook-Pro.local>
2024-03-22 18:47:08 -0700

    made build executable

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh; chmod +x dotnet-install.sh; ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet8; ./dotnet8/dotnet - version; ./dotnet8/dotnet workload install wasm-tools; ./dotnet8/dotnet publish -c Release -o output;
+#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet publish -c Release -o output
